aa166b077ef7808aa536818f4316b0c8adbeb5e8
LucaNotaro <lucakirk9554@gmail.com>
2022-03-24 16:01:18 +0100

    JS-mail-dadi

---
+++ main.js
@@ -0,0 +1,20 @@
+let mail = ["anna@gmail.com", "pietro@gmail.com", "anna@gmail.com","fabio@gmail.com"];
+
+let  verifica = prompt("inserire mail ");
+
+var bol==true;
+
+
+for (let i = 0; i < mail.length; i++) 
+  { 
+    if(mail[i] == verifica)
+    {
+        console.log("entrato nel database");
+    }
+
+    else
+    {
+
+        console.log("la mail non è giusta");
+    }
+  }
